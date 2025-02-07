93bf2021a4d40385a62a0193dd009a77b1e16531
Arif Budianto <reyhan.komputer01@gmail.com>
2025-02-07 21:32:15 +0700

    pergantian image hero section to farm rise

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
       <div className="relative h-[600px]">
         <div className="absolute inset-0">
           <img
-            src="https://plus.unsplash.com/premium_photo-1661964030420-15481be20d5f?q=80&w=2564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src="https://images.pexels.com/photos/2131650/pexels-photo-2131650.jpeg"
             alt="Village landscape"
             className="w-full h-full object-cover"
           />
