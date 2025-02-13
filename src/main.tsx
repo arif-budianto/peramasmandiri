e8f39ec85355fba8fc32c7d93fb4fc5f79f0bd8a
Arif Budianto <reyhan.komputer01@gmail.com>
2025-02-13 16:04:25 +0700

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
+// Import komponen dan fungsi yang diperlukan
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Membuat root aplikasi React dan merender komponen App
+// StrictMode digunakan untuk mendeteksi potensi masalah dalam aplikasi
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
